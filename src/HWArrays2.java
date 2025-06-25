a83ddc8c6aa3b085f88541bb75147da258efd854
awwyxlove <zhanminskzhan@gmail.com>
2025-06-25 14:22:20 +0300

    DZ 5

--- src/HWArrays2.java
+++ src/HWArrays2.java
@@ -6,15 +6,15 @@ public class HWArrays2 {
     public static void main(String[] args) {
         //Task 1
         Random random = new Random();
-        int[][] array = new int [5][2];
+        int[][] array = new int[5][2];
         System.out.println("Первоначальный массив: ");
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = random.nextInt(10,21);
+                array[i][j] = random.nextInt(10, 21);
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
-            }
+        }
         System.out.println("Введите число, которое нужно прибавить к эллементам массива");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
@@ -35,9 +35,9 @@ public class HWArrays2 {
         String[][] chess = new String[8][8];
         for (int b = 0; b < chess.length; b++) {
             for (int w = 0; w < chess[b].length; w++) {
-                if ((b+w)%2 != 0){
+                if ((b + w) % 2 != 0) {
                     chess[b][w] = "B";
-                }else
+                } else
                     chess[b][w] = "W";
                 System.out.print(chess[b][w] + " ");
             }
@@ -50,7 +50,7 @@ public class HWArrays2 {
         int a = scanner1.nextInt();
         int b = scanner1.nextInt();
         int c = 1;
-        int[][] snake = new int [a][b];
+        int[][] snake = new int[a][b];
         for (int z = 0; z < a; z++) {
             if (z % 2 == 0) {
                 for (int x = 0; x < b; x++) {
