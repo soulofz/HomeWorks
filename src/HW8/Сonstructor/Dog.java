4bfa88fdbcbbab6afd44f88445440530cdc513df
awwyxlove <zhanminskzhan@gmail.com>
2025-07-09 23:32:20 +0300

    DZ 8

---
+++ src/HW8/Сonstructor/Dog.java
@@ -0,0 +1,19 @@
+package HW8.Сonstructor;
+
+public class Dog {
+    //Способ 1:
+    //Данные по типу числа пи(вызываются без констуруктора)
+    public static final String EAT = "Meat.";
+    public static final String VOICE = "Woof.";
+
+    // Приватный конструктор(чтобы нельзя было создать.)
+   private Dog(){
+   }
+   //Публичные методы(без обращения к объекту)
+   public static void eat(){
+        System.out.println("Dog likes eat meat.");
+   }
+   public static void voice(){
+       System.out.println("Dog says Woof!");
+   }
+}
---
+++ src/HW8/Сonstructor/Main.java
@@ -0,0 +1,11 @@
+package HW8.Сonstructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog.voice();
+        Dog.eat();
+        System.out.println(Dog.EAT);
+        System.out.println(Dog.VOICE);
+    }
+
+}
