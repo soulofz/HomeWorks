65daa728fd81b2d31acb5e03f79589ae2f13dd45
awwyxlove <zhanminskzhan@gmail.com>
2025-09-04 12:22:22 +0300

    HW 23 edited Main

--- src/main/java/HW23/Main.java
+++ src/main/java/HW23/Main.java
@@ -1,5 +1,12 @@
 package HW23;
 
+import HW23.Model.Sonnet;
+import HW23.Parsers.DOM.MainDom;
+import HW23.Parsers.SAX.MainSax;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +14,31 @@ public class Main {
         System.out.println("Выберите формат парсинга 1 (SAX) или 2 (DOM) :");
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
-        if (input == 1){
-            System.out.println("1");
-        }else if (input == 2){
-            System.out.println("2");
-        } else {
-            System.out.println("Неверное число.");
+        try {
+            Sonnet sonnet = null;
+            if (input == 1) {
+                sonnet = MainSax.parse();
+            } else if (input == 2) {
+                sonnet = MainDom.parse();
+            } else {
+                System.out.println("Неверное число.");
+            }
+            String fileName = sonnet.getAuthor().getFirstName() + "_"
+                    + sonnet.getAuthor().getLastName() + "_"
+                    + sonnet.getTitle() + ".txt";
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                List<String> lines = sonnet.getLines();
+                for (String line : lines) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+            System.out.println("Файл создан.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
